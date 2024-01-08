9a42f1ed2c51086365ec2511c8d583834a0066d9
Edwin waweru <waweruedwin8@Eddyweru.41.212.0.100>
2024-01-09 01:46:12 +0300

    data structures

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    my_list = [1, 2, 3, 4, 5]
-    for num in my_list:
-        print("{}".format(num))
-
-
-print_list_integer()
+    for num in range(len(my_list)):
+        print("{:d}".format(my_list[num]))
