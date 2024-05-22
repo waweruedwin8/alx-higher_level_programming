257f028d06453fb6f1bc57eb21b12939ee1f1367
waweruedwin8 <waweruedwin8@gmail.com>
2024-05-23 00:47:40 +0300

    export to json

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,18 +1,5 @@
-#!/usr/bin/env node
-
+#!/usr/bin/node
 const fs = require('fs');
-
-if (process.argv.length < 3) {
-  console.error('Usage: ./0-readme.js <file_path>');
-  process.exit(1);
-}
-
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(data);
-  }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
