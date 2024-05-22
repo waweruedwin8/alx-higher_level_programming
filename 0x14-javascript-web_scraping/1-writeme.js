722e5badfb6828b4c76438236b96c38309e06b54
waweruedwin8 <waweruedwin8@gmail.com>
2024-05-23 01:20:37 +0300

    json

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) {
+    console.log(error);
+  }
 });
